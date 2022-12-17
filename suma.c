3ac26b5e154a2dcfb211ff22c7f28dc2a412ad33
Wojjtas20 <wojkac002@pbs.edu.pl>
2022-12-17 17:45:22 +0100

    dodanie kodu sumy

---
+++ suma.c
@@ -0,0 +1,9 @@
+ case 1: {
+                float liczba1, liczba2, wynik;
+                printf("Podaj 2 liczby do obliczenia: \n");
+                scanf("%f", &liczba1);
+                scanf("%f", &liczba2);
+        wynik = liczba1 + liczba2;
+        printf("Suma liczb = %.1f", liczba1 + liczba2);
+        break;
+    }
